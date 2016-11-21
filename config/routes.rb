1be0cec87d851d2d887f6f7f2fba7ade15c270a2
Akihiro Nakao <genn777f3@gmail.com>
2016-11-21 15:19:57 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-  root 'static_pages/home'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   #   end
   #   resources :posts, concerns: :toggleable
   #   resources :photos, concerns: :toggleable
- 
+
   # Example resource route within a namespace:
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController

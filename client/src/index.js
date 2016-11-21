71c95ef28c5502f66349d1c5762173e0ab0c762f
Akihiro Nakao <genn777f3@gmail.com>
2016-11-21 15:10:21 +0900

    set hello redux

---
+++ client/src/index.js
@@ -0,0 +1,5 @@
+import { createStore } from 'redux'
+
+let store = createStore(function() { return 'Hello Redux'})
+var contents = document.getElementById('contents')
+contents.innerHTML = store.getState().toString()

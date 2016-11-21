c98b64fdc6b69670488da8ef6ea88645b07c4266
Akihiro Nakao <genn777f3@gmail.com>
2016-11-21 15:15:39 +0900

    set hello redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,3 +3,6 @@ import { createStore } from 'redux'
 let store = createStore(function() { return 'Hello Redux'})
 var contents = document.getElementById('contents')
 contents.innerHTML = store.getState().toString()
+
+// redux ｎ createStoreという関数を使用してStoreを用意し、
+// Storeから値を取得するとHello reduxという値を返却しています
